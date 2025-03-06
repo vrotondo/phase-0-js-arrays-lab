cecddeaf66fe77f799e4855b1b52a093251b70e5
vrotondo <150374510+vrotondo@users.noreply.github.com>
2025-03-06 13:00:50 -0500

    Update inventoryManagement.js

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,16 +1,4 @@
 // Write your code here
-
-
-
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
-
 const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
 function logFirstProduct(name) {
@@ -37,3 +25,14 @@ function updateProductName(name) {
 function removeLastProduct(name) {
   products.pop()
 }
+
+
+
+// Export the necessary parts for testing
+module.exports = {
+  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
+  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
+  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
+  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+  products
+};
